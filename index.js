cff9678a7789ff0355f4d12a6ac4d94b724175b7
geoffrey woodcock <theatomproject@gmail.com>
2019-05-19 19:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,20 @@ class Driver {
   }
 }
 
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
 class Route {
   constructor(beginningLocation, endingLocation){
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
+  blocksTraveled
 }
